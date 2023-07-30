1955d09d6e7750a4f0b57f1ed9857f447b9edf9b
Hans Auzian C <h.auzian@gmail.com>
2023-07-29 21:18:13 -0400

    nvim: flash plugin

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,29 @@
+local status_ok, flash = pcall(require, "flash")
+if not status_ok then
+	return
+end
+
+flash.setup({
+    search = {
+        exclude = {
+            "notify",
+            "cmp_menu",
+            "noice",
+            "flash_prompt",
+            "nvimtree"
+        },
+    },
+    modes = {
+        search = {
+            highlight = {
+                backdrop = true
+            }
+        }
+    },
+})
+
+vim.api.nvim_set_hl(0, 'FlashMatch', { fg = 'green' })
+vim.api.nvim_set_hl(0, 'FlashCurrent', { fg = 'red' })
+vim.api.nvim_set_hl(0, 'FlashLabel', { fg = 'white' })
+
+vim.keymap.set({"n", "v"}, "s", function() require("flash").jump() end)
