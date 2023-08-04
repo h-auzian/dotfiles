a69fd4073e5214e6d978f44001589470648c56b4
Hans Auzian C <h.auzian@gmail.com>
2023-08-04 15:50:58 -0400

    scripts: tmux-fzf-session small adjustments

--- scripts/tmux-fzf-session.sh
+++ scripts/tmux-fzf-session.sh
@@ -1,41 +1,47 @@
 # Creates a new tmux session for a path selected either via fzf or passed as a
 # parameter.
 
-# Normally using alt+c with fzf lists all folders under the current working
-# directory, but it usually takes several seconds. With this script, only an
-# specific group of folders are shown, which are then passed to fzf to allow
-# for a quick selection.
-
-# This script works best while associated to a tmux keybind.
-
-if [[ $# -eq 1 ]]; then
+# First, define a list of paths to pass to fzf, and then get the name of the
+# selected path. Normally using alt+c with fzf lists all folders under the
+# current working directory, but that usually takes several seconds. In this
+# case, only an specific group of paths are selected, which are then passed
+# to fzf to allow for their quick selection.
+if [ $# -eq 1 ]; then
     selected_path=$1
 else
     folders="/home/hans/Dropbox/\n"
-    folders+="/home/hans/scripts/\n"
-    folders+=$(find ~/Hans/Pega/ -mindepth 1 -maxdepth 1 -type d)"\n"
+    folders+="/home/hans/.config/scripts/\n"
+    folders+=$(find ~/Hans/Pega/ -mindepth 1 -maxdepth 3 -type d)"\n"
     folders+=$(find ~/Hans/Proyectos/Programación/ -mindepth 1 -maxdepth 2 -type d)"\n"
     selected_path=`printf "$folders" | fzf`
 fi
 
-if [[ -z $selected_path ]]; then
+if [ -z $selected_path ]; then
     exit 0
 fi
 
+# Define the name of the session using the name of the selected folder plus the
+# name of its parent. If the parent folder is home, that is, the user name,
+# then just use the name of the selected folder.
 parent_dir=$(basename $(dirname "$selected_path"))
 selected_dir=$(basename "$selected_path")
 
-if [[ $parent_dir != "hans" ]]; then
+if [ $parent_dir != $USER ]; then
     session_name=`echo $parent_dir / $selected_dir`
 else
     session_name=$selected_dir
 fi
 
+# If a session with said name doesn't exists, create a new tmux session with
+# four windows, open nvim in the first one, and then switch to said session.
+# If the session already exists, then simply switch to it without creating
+# anything.
 if ! tmux has-session -t="$session_name" 2> /dev/null; then
     tmux new-session -ds "$session_name" -c $selected_path
     tmux new-window -t "$session_name" -c $selected_path
     tmux new-window -t "$session_name" -c $selected_path
     tmux new-window -t "$session_name" -c $selected_path
+    tmux send-keys -t "$session_name":0 'nvim' Enter
     tmux switch-client -t "$session_name":0
 else
     tmux switch-client -t "$session_name"
