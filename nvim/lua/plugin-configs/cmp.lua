cbb3cf9998d9c56247364610a2c03b50294da0eb
Hans Auzian C <h.auzian@gmail.com>
2024-01-13 18:50:04 -0300

    nvim: cmp parameter jump keybindings

--- nvim/lua/plugin-configs/cmp.lua
+++ nvim/lua/plugin-configs/cmp.lua
@@ -1,13 +1,34 @@
-local status_ok, cmp = pcall(require, "cmp")
+local cmp_status_ok, cmp = pcall(require, "cmp")
+local luasnip_status_ok, luasnip = pcall(require, "luasnip")
 
-if not status_ok then
+if not cmp_status_ok or not luasnip_status_ok then
 	return
+end
+
+local function expand_next(fallback)
+    if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+    else
+        fallback()
+    end
+end
+
+local function expand_prev(fallback)
+    if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+    else
+        fallback()
+    end
 end
 
 cmp.setup({
     mapping = {
-        ["<C-l>"] = cmp.mapping.confirm({select = true}),
-        ["<C-o>"] = cmp.mapping.confirm({select = true}),
-        ["<Tab>"] = cmp.config.disable,
+        ["<C-o>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-j>"] = cmp.mapping(expand_prev, { "i", "s" }),
+        ["<C-l>"] = cmp.mapping(expand_next, { "i", "s" }),
     }
 })
+
+-- Disable default keybinds in insert mode to avoid conflicts with the CMP keybinds.
+vim.keymap.set("i", "<C-j>", "")
+vim.keymap.set("i", "<C-l>", "")
