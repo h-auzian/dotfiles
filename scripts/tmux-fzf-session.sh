976492bca9ccfc697080b849dd2edcd203e63769
Hans Auzian C <h.auzian@gmail.com>
2024-01-19 19:40:32 -0300

    scripts: allow passing the amount of windows and the initial window index to newly created tmux sessions via fzf

--- scripts/tmux-fzf-session.sh
+++ scripts/tmux-fzf-session.sh
@@ -1,21 +1,36 @@
 # Creates a new tmux session for a path selected either via fzf or passed as a
 # parameter.
 
-# First, define a list of paths to pass to fzf, and then get the name of the
-# selected path. Normally using alt+c with fzf lists all folders under the
-# current working directory, but that usually takes several seconds. In this
-# case, only an specific group of paths are selected, which are then passed
-# to fzf to allow for their quick selection.
-if [ $# -eq 1 ]; then
+# Retrieve possible parameters.
+total_windows=4
+initial_window=1
+
+if [ $# -ge 1 ]; then
     selected_path=$1
+
+    if [ $# -ge 2 ]; then
+        total_windows=$2
+    fi
+
+    if [ $# -ge 3 ]; then
+        initial_window=$3
+    fi
+
+# If no path was received as parameter, define a list of paths to pass to fzf,
+# and then get the name of the selected path. Normally using Alt+C with fzf
+# lists all folders under the current working directory, but that usually takes
+# several seconds. In this case, only an specific group of paths are selected,
+# which are then passed to fzf to allow for their quick selection.
 else
-    folders="/home/hans/Dropbox/\n"
-    folders+="/home/hans/.config/scripts/\n"
+    folders="$HOME/Dropbox/\n"
+    folders+="$HOME/.ssh/\n"
+    folders+=$(find ~/.config/ -mindepth 1 -maxdepth 1 -type d)"\n"
     folders+=$(find ~/Hans/Pega/ -mindepth 1 -maxdepth 3 -type d)"\n"
     folders+=$(find ~/Hans/Proyectos/Programación/ -mindepth 1 -maxdepth 2 -type d)"\n"
     selected_path=`printf "$folders" | fzf`
 fi
 
+# If no path was selected with fzf, stop.
 if [ -z $selected_path ]; then
     exit 0
 fi
@@ -36,17 +51,25 @@ fi
 # This can happen in paths such as ".config", ".ssh", and so on.
 session_name=$(echo $session_name | tr -d .)
 
-# If a session with said name doesn't exists, create a new tmux session with
-# four windows, open nvim in the first one, and then switch to said session.
-# If the session already exists, then simply switch to it without creating
-# anything.
+# If a session with said name doesn't exist, create it according to the
+# received parameters.
 if ! tmux has-session -t="$session_name" 2> /dev/null; then
+
+    # Create the session.
     tmux new-session -ds "$session_name" -c $selected_path
-    tmux new-window -t "$session_name" -c $selected_path
-    tmux new-window -t "$session_name" -c $selected_path
-    tmux new-window -t "$session_name" -c $selected_path
+
+    # Create the specified number of windows.
+    for i in $(seq 2 $total_windows); do
+        tmux new-window -t "$session_name" -c $selected_path
+    done
+
+    # Open nvim on the first window.
     tmux send-keys -t "$session_name":1 'nvim' Enter
-    tmux switch-client -t "$session_name":1
+
+    # Switch to the newly created session on the specified window.
+    tmux switch-client -t "$session_name":$initial_window
+
+# If the session already exists, then simply switch to it.
 else
     tmux switch-client -t "$session_name"
 fi
