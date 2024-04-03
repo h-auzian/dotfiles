82a50a00bfddc343744411da7f1085379ca99be8
Hans Auzian C <h.auzian@gmail.com>
2024-04-03 16:40:48 -0300

    nvim: additional kanagawa theme overrides

--- nvim/lua/theme-overrides/init.lua
+++ nvim/lua/theme-overrides/init.lua
@@ -1,6 +1,7 @@
 -- Tries to load a lua script with the same name as the currently selected
 -- theme, in order to apply possible theme overrides if the flle exists.
--- To check the current highlight groups that can be overridden, run :hi.
+-- To check the highlight groups that can be overridden, run ":hi".
+-- To filter highlight groups by name, run ":filter <search> hi".
 
 local color_scheme = vim.g.colors_name
 pcall(require, "theme-overrides." .. color_scheme)
