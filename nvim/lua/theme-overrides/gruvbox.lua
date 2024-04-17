f11e8e6d5fa1e0387e4032ffdb5dbcb71bc75c33
Hans Auzian C <h.auzian@gmail.com>
2024-04-17 13:14:28 -0400

    nvim: gruvbox theme override

---
+++ nvim/lua/theme-overrides/gruvbox.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#808080" })
+vim.api.nvim_set_hl(0, "Comment", { fg = "#B2A394", italic = true })
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = nil })
+vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = nil, fg = nil })
+vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { fg = "#c1c535", bg = "#202020" })
+vim.api.nvim_set_hl(0, "TelescopeSelection", { fg = "#c1c535", bg = "#202020" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = nil })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = nil })
