7a5d1364990ba411aa2729928b3d38e48d4cb4fe
Hans Auzian C. <h.auzian@gmail.com>
2024-08-07 17:00:46 -0400

    autokey: remove unused commands

--- autokey/data/Commands/Applications/Tmux/Execute last command on second window and go back to first window.py
+++
@@ -1,4 +0,0 @@
-keyboard.send_keys("<alt>+<shift>+i")
-keyboard.send_keys("<up>")
-keyboard.send_keys("<enter>")
-keyboard.send_keys("<alt>+<shift>+u")
--- autokey/data/Commands/Applications/Tmux/Repeat last command.py
+++
@@ -1,3 +0,0 @@
-keyboard.send_keys("q")
-keyboard.send_keys("<up>")
-keyboard.send_keys("<enter>")
