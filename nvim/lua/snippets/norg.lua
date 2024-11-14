fd7957bcb4af33f59c5e3d9b3b690f2f94057769
Hans Auzian C. <h.auzian@gmail.com>
2024-11-14 14:30:52 -0300

    nvim: Neorg additional snippets

--- nvim/lua/snippets/norg.lua
+++ nvim/lua/snippets/norg.lua
@@ -6,6 +6,23 @@ local code_snippet_format = [[
     @end
 ]]
 
+local star_format = [[
+    - S: {}
+    - T: 
+    - A: 
+    - R: 
+]]
+
+local problem_format = [[
+    ** {}:
+    *** Description:
+        - 
+    *** Observations:
+        - 
+    *** Solution:
+        - 
+]]
+
 local code_snippet = ls.s(
     "code-",
     fmt(code_snippet_format, {
@@ -13,9 +30,25 @@ local code_snippet = ls.s(
     })
 )
 
+local star_snippet = ls.s(
+    "star-",
+    fmt(star_format, {
+        ls.i(1),
+    })
+)
+
+local problem_snippet = ls.s(
+    "pr-",
+    fmt(problem_format, {
+        ls.i(1),
+    })
+)
+
 local snippets = {}
 local autosnippets = {
     code_snippet,
+    star_snippet,
+    problem_snippet,
 }
 
 return snippets, autosnippets
