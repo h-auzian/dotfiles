80e40e51f0cc2491aa4223c4777d36b08aa17b12
Hans Auzian C. <h.auzian@gmail.com>
2025-04-18 22:58:30 -0400

    nvim: additional c++ snippets

--- nvim/lua/snippets/cpp.lua
+++ nvim/lua/snippets/cpp.lua
@@ -1,3 +1,5 @@
+local ls = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
 local helpers = require("functions.snippet-helpers")
 
 local print_snippet = helpers.create_print_snippet(
@@ -8,10 +10,29 @@ local print_variable_snippet = helpers.create_print_variable_snippet(
     'std::cout << "{}: " << {} << std::endl;'
 )
 
+local doc_comment_snippet = helpers.create_doc_comment_snippet([[
+    /**
+     * {}
+     */
+]])
+
+local pragma_once_snippet = ls.s(
+    "pra-",
+    fmt("#pragma once", {})
+)
+
+local iostream_snippet = ls.s(
+    "io-",
+    fmt("#include <iostream>", {})
+)
+
 local snippets = {}
 local autosnippets = {
     print_snippet,
     print_variable_snippet,
+    doc_comment_snippet,
+    pragma_once_snippet,
+    iostream_snippet,
 }
 
 return snippets, autosnippets
