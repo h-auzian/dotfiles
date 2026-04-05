21710a6457aba5edb463e80f42f93c23d6a2d79d
Hans Auzian C. <h.auzian@gmail.com>
2026-04-04 23:33:58 -0300

    yazi: initial config

---
+++ yazi/init.lua
@@ -0,0 +1,7 @@
+function Linemode:custom_linemode()
+    local time = math.floor(self._file.cha.mtime or 0)
+    local time_str = time > 0 and os.date("%d %b %Y - %H:%M", time) or ""
+    local size = self._file:size()
+    local size_str = size and ya.readable_size(size) or ""
+    return ui.Line(string.format("%s | %s", size_str, time_str))
+end
