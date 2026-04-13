1b4e54800a71758a7d2e33bc0ae38c0ed1c6083a
Hans Auzian C. <h.auzian@gmail.com>
2026-04-13 00:07:40 -0400

    nvim: qmk plugin

---
+++ nvim/lua/plugins/qmk.lua
@@ -0,0 +1,14 @@
+return {
+    "codethread/qmk.nvim",
+    opts = {
+        name = "lily58",
+        variant = "zmk",
+        layout = {
+            'x x x x x x _ _ _ x x x x x x',
+            'x x x x x x _ _ _ x x x x x x',
+            'x x x x x x _ _ _ x x x x x x',
+            'x x x x x x x _ x x x x x x x',
+            '_ _ x x x x _ _ _ x x x x _ _',
+        },
+    },
+}
