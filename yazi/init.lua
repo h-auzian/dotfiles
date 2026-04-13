e8ccb5c66cb9ed96bd0560ca28d477c81533b945
Hans Auzian C. <h.auzian@gmail.com>
2026-04-12 23:47:25 -0400

    yazi: additional keybinds and options

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,4 +1,4 @@
-function Linemode:custom_linemode()
+function Linemode:custom()
     local time = math.floor(self._file.cha.mtime or 0)
     local time_str = time > 0 and os.date("%d %b %Y - %H:%M", time) or ""
     local size = self._file:size()
@@ -6,4 +6,9 @@ function Linemode:custom_linemode()
     return ui.Line(string.format("%s | %s", size_str, time_str))
 end
 
-require("fs-usage"):setup()
+require("fs-usage"):setup({
+    bar = false,
+    warning_threshold = -1,
+    style_normal = { bg = "#303030" },
+    padding = { open = "█", close = "█" },
+})
