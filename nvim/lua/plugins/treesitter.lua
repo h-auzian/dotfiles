f54cea0154a867589fec066889864883d3dcbbbb
Hans Auzian C. <h.auzian@gmail.com>
2026-07-16 13:43:56 -0400

    neovim: devicetree installation by default for treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -9,6 +9,7 @@ return {
                 "c",
                 "cmake",
                 "cpp",
+                "devicetree",
                 "glsl",
                 "go",
                 "javascript",
